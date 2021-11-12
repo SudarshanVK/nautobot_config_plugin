7e1e2161522d4a66fa941620338e2bfbe756e9a6
Mr Sudarshan Vijaya Kumar <sudarshan.net09@gmail.com>
2021-11-12 15:10:15 +1100

    testing a nornir inventory in job

---
+++ jobs/nrjob.py
@@ -0,0 +1,40 @@
+from nautobot.extras.jobs import Job, MultiObjectVar, BooleanVar
+
+from nornir import InitNornir
+from nornir.core.plugins.inventory import InventoryPluginRegister
+from nautobot_plugin_nornir.constants import NORNIR_SETTINGS
+
+def init_nornir():
+    """
+    Initialize Nautobot Inventory
+    """
+    
+    nr = InitNornir(
+        runner=NORNIR_SETTINGS.get("runner"),
+        logging={"enabled": False},
+        inventory={
+            "plugin": "nautobot-inventory",
+            "options": {
+                "credentials_class": NORNIR_SETTINGS.get("credentials"),
+                "params": NORNIR_SETTINGS.get("inventory_params"),
+                "queryset": get_job_filter(data),
+                # "defaults": {"now": now},
+            },
+        },
+    )
+
+    return nr
+
+class NrJob(Job):
+    """
+    Nautobot Job
+    """
+
+    def run(self):
+        """
+        Run Nautobot Job
+        """
+        nr = init_nornir()
+        self.log_info(obj= None, message=f"{nr}")
+
+        return result
